25700b512d277da5b53545478f84090cb906d3d2
Jeff Ching <chingor@google.com>
2026-09-16 21:13:51 +0000

    refactor(auth)!: rename GoogleCloudAuth to GoogleAuth (#920)
    Fixes #905

--- Package.swift
+++ Package.swift
@@ -63,7 +63,7 @@ let package = Package(
           name: "GoogleAppsScriptTypeSheets", package: "swift-google-apps-script-type-sheets"),
         .product(
           name: "GoogleAppsScriptTypeSlides", package: "swift-google-apps-script-type-slides"),
-        .product(name: "GoogleCloudAuth", package: "swift-google-auth"),
+        .product(name: "GoogleAuth", package: "swift-google-auth"),
         .product(name: "GoogleCloudGax", package: "swift-google-gax"),
         .product(name: "GoogleCloudWKT", package: "swift-google-wkt"),
         .product(name: "Logging", package: "swift-log"),
